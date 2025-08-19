787419c67256b6eee6806fee5b6078873dff2509
swatiparge <swati23793@gmail.com>
2025-08-19 10:48:39 +0530

    feat: favicon icon updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  output: "export",      // replaces next export
-  basePath: "/snappy",   // repo name
-  assetPrefix: "/snappy/",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
